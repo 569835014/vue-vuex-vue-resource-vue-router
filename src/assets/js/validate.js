fda9fc5c81d184feae4e3b2bcf8f8cab4adeee13
569835014 <569835014@qq.com>
2016-12-07 10:40:37 +0800

    封装验证方法

--- src/assets/js/validate.js
+++ src/assets/js/validate.js
@@ -18,6 +18,14 @@ let validate={
       msg:"用户名格式错误",
       result:false,
       ruleList:[/^1(3|4|5|7|8)\d{9}$/, /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/]
+    },
+    idCard:{
+      ev:"input",
+      type:"idCard",
+      symbol:"or",
+      msg:"你输入的身份证不正常",
+      result:false,
+      ruleList:[/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/,/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X|x)$/]
     }
   },
   vali:{}
@@ -25,39 +33,43 @@ let validate={
 validate.install = function (Vue, options) {
   var that=this;
   // 1. 添加全局方法或属性
-  Vue.myGlobalValidate ={
-    ev:"click",
-    type:"",
-    msg:""
-  };
+  // Vue.myGlobalValidate ={
+  //   ev:"click",
+  //   type:"",
+  //   msg:""
+  // };
   // 2. 添加全局资源
   Vue.directive('validate', {
-    twoWay: true,
     bind:function (el, binding, vnode) {
       var opt=binding.value;
       if(el.tagName.toLocaleLowerCase()!="input"){
         el=el.getElementsByTagName("input")[0]
       }
-      var rule=that.rules[opt.type];
-      var vm=vnode.context.callObj[opt.type];
-      console.info(vm)
+      var rule={};
+      Object.assign(rule,that.rules[opt.type]);
+      var vm="";
+      if(opt.namespace){
+        vm=vnode.context.validateResult[opt.namespace];
+      }else{
+        vm=vnode.context.validateResult[opt.type];
+      }
       if(!rule){
         that.rules[opt.type]=opt;
         rule=opt;
       }else{
-        Object.assign(that.rules[opt.type],opt);
+        Object.assign(rule,opt);
       }
+      console.info(rule);
       var list=rule.ruleList;
       el.addEventListener(opt.ev,validateMet,true);
       function validateMet() {
         var val=el.value;
+        rule.result=false;
         for(var i=0,l=list.length;i<l;i++){
           var reg=list[i];
           if(rule.symbol=="and"){
             if(!reg.test(val)){
-
               console.info("无效");
-              alert("无效");
               break
             }else{
               if(i==l-1){
@@ -66,26 +78,23 @@ validate.install = function (Vue, options) {
                 }
               }
             }
-
-
           }else{
             if(reg.test(val)){
               rule.result=true;
               console.info("有效");
-              alert("有效");
               break
             }
           }
         }
+        vm.flag=rule.result;
         if(!rule.result){
-          vm.flag=true;
-          vm.text=rule.msg;
+          if(!vm.msg){
+            vm.msg=rule.msg;
+          }
         }
       }
     },
     update:function (oldValue,value) {
-      var self = this;
-      console.info(self)
     },
   });
   // 3. 添加实例方法
--- src/view/Index.vue
+++ src/view/Index.vue
@@ -5,9 +5,9 @@
         <mu-icon-button icon='expand_more' slot="right"/>
       </mu-appbar>
       <div>{{test}}</div>
-      <input type="text" v-validate="userName"><span icon="phone" v-if="callObj.userName.flag">{{callObj.userName.text}}</span>
-
-      <mu-text-field label="请输入邮箱" hintText="请输入邮箱" type="text" icon="phone" errorText="" :maxLength="10" errorColor="red" labelFloat v-validate="{'ev':'change',type:'email'}"/>
+      <input type="text" v-validate="userName"><span icon="phone" v-if="!validateResult.userName.flag">{{validateResult.userName.msg}}</span>
+      <input type="text" v-validate="{'ev':'change',type:'idCard'}"><span icon="phone" v-if="!validateResult.idCard.flag">{{validateResult.idCard.msg}}</span>
+      <mu-text-field label="请输入邮箱" hintText="请输入邮箱" type="text" icon="email" :errorText="validateResult.email2.flag?'':validateResult.email2.msg" :maxLength="10" errorColor="red" labelFloat v-validate="{'ev':'change',type:'email','namespace':'email2'}"/>
     </div>
 </template>
 <style>
@@ -21,17 +21,33 @@
     data(){
       return {
          userName:{
-          ev:"change",
+          ev:"input",
           type:"userName",
           symbol:"or",
           ruleList:[/^1(3|4|5|7|8)\d{9}$/, /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/]
         },
          test:"a",
-         callObj:{
-           userName:{
+        validateResult:{
+          userName:{
              flag:false,
-             text:"",
-           }
+             msg:"",
+           },
+          idCard:{
+            flag:false,
+            msg:"",
+          },
+          email:{
+             flag:false,
+             msg:""
+           },
+          email1:{
+            flag:false,
+            msg:""
+          },
+          email2:{
+            flag:false,
+            msg:""
+          },
          }
       }
     },
